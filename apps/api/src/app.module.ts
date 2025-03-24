aa6dd3d28f5688dd5d27866b346a8f1af2bca274
Luca Palmarucci <lpalmarucci@gmail.com>
2025-03-24 14:40:03 +0100

    feat(api): setup player resources

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, Reflector } from '@nestjs/core';
 import { ClerkAuthGuard } from './auth/clerk-auth.guard';
+import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PlayerModule],
   controllers: [AppController],
   providers: [AppService],
 })
