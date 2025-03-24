73447263b1dda888fdbc3e8c0c98963c870dac34
Luca Palmarucci <lpalmarucci@tierratelematics.com>
2025-03-24 17:24:31 +0100

    feat(api): installed @workspace/types

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,15 +1,14 @@
-import { Inject, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD, Reflector } from '@nestjs/core';
-import { ClerkAuthGuard } from './auth/clerk-auth.guard';
 import { PlayerModule } from './player/player.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PlayerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
