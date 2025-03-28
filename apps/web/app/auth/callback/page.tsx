7f5fd03fc3a5a5a5480509b35736094a8c7a0fa8
Luca Palmarucci <lpalmarucci@tierratelematics.com>
2025-03-28 18:41:35 +0100

    fix(web): wrap GoogleAuthCallback with suspense boundary

--- apps/web/app/auth/callback/page.tsx
+++ apps/web/app/auth/callback/page.tsx
@@ -1,9 +1,10 @@
 import { GoogleAuthCallback } from "@/features/auth/components/google-auth-callback";
+import { Suspense } from "react";
 
 export default async function Page() {
   return (
-    <>
+    <Suspense fallback={<span>Error google auth</span>}>
       <GoogleAuthCallback />
-    </>
+    </Suspense>
   );
 }
