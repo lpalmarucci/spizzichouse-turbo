3e32cf4b27a6e7de6ed63f7e487e8398be0fe5d2
Luca Palmarucci <lpalmarucci@tierratelematics.com>
2025-04-10 17:18:11 +0200

    fix(api): unit tests

--- apps/api/src/match/match.controller.spec.ts
+++ apps/api/src/match/match.controller.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MatchController } from './match.controller';
 import { MatchService } from './match.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { PlayersModule } from '../players/players.module';
 
 describe('MatchController', () => {
   let controller: MatchController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PlayersModule],
       controllers: [MatchController],
-      providers: [MatchService],
+      providers: [MatchService, PrismaService],
     }).compile();
 
     controller = module.get<MatchController>(MatchController);
--- apps/api/src/match/match.service.spec.ts
+++ apps/api/src/match/match.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MatchService } from './match.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { PlayersModule } from '../players/players.module';
 
 describe('MatchService', () => {
   let service: MatchService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MatchService],
+      imports: [PlayersModule],
+      providers: [MatchService, PrismaService],
     }).compile();
 
     service = module.get<MatchService>(MatchService);
