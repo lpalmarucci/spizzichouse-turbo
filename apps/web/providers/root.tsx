9f34d18bb61539d653f882274f790830ff92e621
Luca Palmarucci <lpalmarucci@tierratelematics.com>
2025-04-18 13:00:53 +0200

    chore(web): removed @tanstack/query dependency

--- apps/web/providers/root.tsx
+++ apps/web/providers/root.tsx
@@ -1,14 +1,11 @@
 import { PropsWithChildren } from "react";
 import { ThemeProvider } from "@/providers/shadcn";
-import { TanstackQueryProvider } from "@/providers/query";
 import { ApolloWrapper } from "@/providers/apollo";
 
 export async function RootProvider({ children }: PropsWithChildren) {
   return (
     <ApolloWrapper>
-      <TanstackQueryProvider>
-        <ThemeProvider>{children}</ThemeProvider>
-      </TanstackQueryProvider>
+      <ThemeProvider>{children}</ThemeProvider>
     </ApolloWrapper>
   );
 }
